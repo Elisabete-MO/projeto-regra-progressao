84a291d0da6e961cf40292d5bb3240c0bfe5b181
Bete.mrt <bete.mrt@gmail.com>
2023-07-12 17:19:36 -0300

    final grade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -34,5 +34,20 @@ public class App {
       activitiesGrades[i] = Short.parseShort(activityGrade);
     }
     scanner.close();
+
+    double data = 0.0;
+    for (int i = 0; i < activityNumber; i++) {
+      data += (activitiesGrades[i] * activitiesWeights[i]);
+    }
+    double finalGrade = data / 100.0;
+
+    if (finalGrade >= 85.0) {
+      System.out.println("Parabéns! Você alcançou" + finalGrade
+          + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação "
+          + "alcançada neste período, " + finalGrade
+          + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    }
   }
 }
