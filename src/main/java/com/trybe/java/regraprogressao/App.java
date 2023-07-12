891f49ed8ea765abad1ddb9af800bd9cc6462ac4
Bete.mrt <bete.mrt@gmail.com>
2023-07-12 15:13:12 -0300

    input

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,22 @@ public class App {
    * Metodo main. Here we go
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int activity_number = scanner.nextInt();
+
+    String[] activity_names = new String[activity_number];
+    int[] activity_weights = new int[activity_number];
+
+    for (int i = 0; i < activity_number; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String activity_name = scanner.nextLine();
+      activity_names[i] = activity_name;
+
+      System.out.println("Digite o peso da atividade " + i + ":");
+      int activity_weight = scanner.nextInt();
+      activity_weights[i] = activity_weight;
+    }
+    scanner.close();
   }
 }
